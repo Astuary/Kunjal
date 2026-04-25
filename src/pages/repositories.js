28cd112b549f0df72712a81eb2c3caadaad1bf85
astuary <kunjalspanchal@gmail.com>
2026-04-24 20:39:26 -0400

    Fixing art paths

--- src/pages/repositories.js
+++ src/pages/repositories.js
@@ -7,24 +7,33 @@ import {RepoGrid, RepoInfo, Avatar} from "../components/styled/repository"
 import {SectionIntro, ContainerLayout} from "../components/common";
 
 const RepositoryPage = ({data}) => { 
-  const {
-    name,
-    avatarUrl,
-    repositories,
-  } = data.githubData.data.viewer
+  const viewer = data.githubData && data.githubData.data && data.githubData.data.viewer
+  const repositories = viewer && viewer.repositories && viewer.repositories.nodes
+    ? viewer.repositories.nodes
+    : []
 
   return (
     <Layout> 
       <SEO title="Github Repositories" />
       <ContainerLayout>
         <SectionIntro>
-          <RepoInfo>
-            <Avatar style={{ backgroundImage: `url(${avatarUrl})` }} />
-            <h2>{name}</h2>
-          </RepoInfo>
-          <RepoGrid>
-            {repositories.nodes.map((repo, index) => <Repository key={index} repo={repo} />).reverse()}
-          </RepoGrid>
+          {viewer ? (
+            <>
+              <RepoInfo>
+                <Avatar style={{ backgroundImage: `url(${viewer.avatarUrl})` }} />
+                <h2>{viewer.name}</h2>
+              </RepoInfo>
+              <RepoGrid>
+                {repositories.map((repo, index) => (
+                  <Repository key={index} repo={repo} />
+                )).reverse()}
+              </RepoGrid>
+            </>
+          ) : (
+            <p>
+              GitHub repositories are temporarily unavailable. Please check back soon.
+            </p>
+          )}
         </SectionIntro>
       </ContainerLayout>
     </Layout>
