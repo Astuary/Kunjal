a6731109b07f95f0a5d2381493ce416f6ad4a23e
astuary <kunjalspanchal@gmail.com>
2026-07-03 15:18:28 -0700

    New art.

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { useStaticQuery, graphql, withPrefix } from "gatsby"
-import {AboutSection, Avatar, Title, Text, SubTitle} from './style';
+import {AboutSection, Avatar, Title, Text} from './style';
 import {SectionIntro, ContainerLayout, ResumeButton} from "../common";
 
 const About = () => {
@@ -17,16 +17,15 @@ const About = () => {
         <ContainerLayout>
           <AboutSection>
             <Avatar
-              src={data.placeholderImage.publicURL}
+              src={withPrefix(data.placeholderImage.publicURL)}
               alt="Kunjal Panchal"
               loading="eager"
               decoding="sync"
             />
             <div className="about-content">
               <Title> Hello, I'm Kunjal </Title>
-              <Text> I'm a PhD candidate at Manning College of Information and Computer Sciences, Universtiy of Massachusetts - Amherst. </Text>
-              <Text> My main area of research is <b className="text-primary lined-link">Machine Learning Training and Inference in Resource-</b> <b className="text-primary lined-link">constrained Environments</b>, advised by Prof. Hui Guan.</Text>
-              <Text> I am currently a part of MLSys lab at UMass.</Text>
+              <Text> I'm a Research Scientist at Adobe, working on the development of on-device, real-time ML models. I finished my PhD from Manning College of Information and Computer Sciences, Universtiy of Massachusetts - Amherst, advised by Prof. Hui Guan at the MLSys lab. </Text>
+              <Text> My main area of research is <b className="text-primary lined-link">Machine Learning Training and Inference in Resource-</b> <b className="text-primary lined-link">constrained Environments</b>.</Text>
               <ResumeButton
                 href={withPrefix("/Kunjal_Panchal_CV_Apr_2026.pdf")}
                 target="_blank"
--- src/components/about/style.js
+++ src/components/about/style.js
@@ -23,9 +23,9 @@ export const Avatar = styled.img`
   border-radius: 5px;
   box-shadow: 0px 0px 64px rgba(0, 0, 0, 0.15);
   width: 100%;
-  height: 100%;
+  height: auto;
   display: block;
-  object-fit: cover;
+  align-self: start;
 `
 export const Title = styled.h1`
   font-size: 3rem;
